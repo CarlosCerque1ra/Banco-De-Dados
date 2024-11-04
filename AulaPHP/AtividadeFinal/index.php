d08fb5410575e7b739d25d098ba9fb46692f387a
Carlos Cerqueira <carlos.a.senai02@gmail.com>
2024-11-04 15:10:59 -0300

    php

--- AulaPHP/AtividadeFinal/index.php
+++ AulaPHP/AtividadeFinal/index.php
@@ -1,68 +1,55 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
     <link rel="stylesheet" href="style.css">
+    <title></title>
 </head>
+
 <body>
-<form method="POST">
-        <input type="text" name="nome" placeholder="digite seu nome" require>
-        <input type="number" name="idade" placeholder="digite sua idade" require>
-        <select name="ingresso" id="vip">
-            <option value="1">Vip</option>
-            <option value="2">Regular</option>
-            <option value="3">Basico</option>
-        </select>
-        <button type="submit">Enviar</button>
-    </form>
+    <main class="Formulario">
+        <form  method="POST">
+            <h2>Formulário de ingressos</h2>
+                <p>Nome:</p>
+            <input type="text" name="nome" placeholder="Digite seu nome" required id="nome">
+            <p>Idade:</p>
+            <input type="number" name="idade" placeholder="Digite sua idade" required id="idade">
+            <p>Opção de ingresso:</p>
+            <select id="opcoes" name="ingresso">
+                <option value="1">Vip</option>
+                <option value="2">Regular</option>
+                <option value="3">Basico</option>
+            </select>
+            <button type="submit"> Enviar </button>
+        </form>
     <?php
-        $ingresso = "nenhum selecionado";
-        $nome = "não inserido";
-        $idade = 0;
-        $preco =0.00;
-        if(isset($_POST["nome"])){
-            $nome = $_POST["nome"];
-        }
-        if(isset($_POST["idade"])){
-            $idade = $_POST["idade"];
-        }
-        if(isset($_POST["ingresso"])){
-            $ingresso = $_POST["ingresso"];
-        }
-        if ($idade < 18){
-            echo "vanda não permitida para menores de idade";
-        }
-        else{
-           switch($ingresso){
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $idade = $_POST['idade'];
+    $nome = $_POST['nome'];
+    $ingresso = $_POST['ingresso'];
+
+    if ($idade < 18) {
+        echo "<p>ingresso não permitido para menores de idade</p>";
+    } else {
+        switch($ingresso) {
             case 1:
-                $ingresso = "Vip";
-                $preco = 100.00;
-            break;
-
+                $preço = 100;
+                break;
             case 2:
-                $ingresso = "Regular";
-                $preco = 50.00;
-            break;
-
+                $preço = 50;
+                break;
             case 3:
-                $ingresso = "Basico";
-                $preco = 20.00;
-            break;
-
+                $preço = 20;
+                break;
             default:
-            exit;
-           } 
-        }
-        if($idade > 18){
-            echo "Nome: $nome <br>";
-            echo "Idade: $idade <br>";
-            echo "Tipo de ingresso: $ingresso Preço: R$" . number_format($preco, 2,',');
-        }
-        else{
-            echo "";
-        }
-    ?>
+            exit; 
+        }  
+        echo "<p>Bem-Vindo, $nome</p>";
+        echo "Preço: R$".number_format ($preço, 2,",");
+    }  
+}     
+    
+?>   
 </body>
-</html>
