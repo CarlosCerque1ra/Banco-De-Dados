012b98db2678c8337a6b71121ef46fcf58a294da
Carlos Cerqueira <carlos.a.senai02@gmail.com>
2024-11-04 15:10:05 -0300

    php

--- AulaPHP/AtividadeFinal/index.php
+++ AulaPHP/AtividadeFinal/index.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
     <link rel="stylesheet" href="style.css">
-    <title>Venda de ingreços</title>
 </head>
 <body>
-    
-    <form method="POST">
+<form method="POST">
         <input type="text" name="nome" placeholder="digite seu nome" require>
         <input type="number" name="idade" placeholder="digite sua idade" require>
         <select name="ingresso" id="vip">
